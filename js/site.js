9b76af8f6cce22d0be5b3f40a50ace5e01958fc5
josephStuff <93104277+josephStuff@users.noreply.github.com>
2022-01-17 17:36:34 -0500

    a save before more editing

---
+++ js/site.js
@@ -0,0 +1,18 @@
+
+
+
+// display message to the user.
+
+function getMessage() {
+
+    let userMessage = document.getElementById("message").value;
+
+    Swal.fire(
+        
+        title: ;
+        userMessage:   ;
+        console.log(userMessage)
+    );
+
+    console.log(userMessage);
+}
